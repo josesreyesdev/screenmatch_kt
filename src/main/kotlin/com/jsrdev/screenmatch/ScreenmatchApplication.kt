e42c6143d77e072d45f2e3a4555e0fc151e915e8
Jose SR <josesanchez09190@gmail.com>
2024-10-11 21:51:51 -0600

    Add project

---
+++ src/main/kotlin/com/jsrdev/screenmatch/ScreenmatchApplication.kt
@@ -0,0 +1,11 @@
+package com.jsrdev.screenmatch
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class ScreenmatchApplication
+
+fun main(args: Array<String>) {
+    runApplication<ScreenmatchApplication>(*args)
+}
---
+++ src/test/kotlin/com/jsrdev/screenmatch/ScreenmatchApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.jsrdev.screenmatch
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class ScreenmatchApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
